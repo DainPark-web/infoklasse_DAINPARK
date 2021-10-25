c21d1ccdc70fa99bac09893c7eacef70d3b51331
dain <57316771+DainPark-web@users.noreply.github.com>
2021-10-25 15:58:53 +0200

    update

--- Coding: First Steps/00_Template/js/ball.js
+++ Coding: First Steps/00_Template/js/ball.js
@@ -1,11 +1,28 @@
 class Ball {
-    constructor(){
+    constructor(WIDTH, HEIGHT){
+        this.x = random(0, WIDTH);
+        this.y = random(0, HEIGHT);
+        this.xSpeed = random(-1, 1);
+        this.ySpeed = random(-1, 1);
 
+        this.size = random(10, 100);
     }
 
     draw(){
         fill(200,0,0);
-        ellipse(50, 50, 100,100);
+        ellipse(this.x, this.y, this.size,this.size);
+    }
+
+    move(){
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        if(this.x > WIDTH || this.x < 0){
+            this.xSpeed *= -1;
+        }
+        if(this.y > HEIGHT || this.y < 0){
+            this.ySpeed *= -1;
+        }
     }
 
 }
--- Coding: First Steps/00_Template/js/sketch.js
+++ Coding: First Steps/00_Template/js/sketch.js
@@ -4,19 +4,30 @@ let HEIGHT = window.innerHeight;
 let time = 0;
 
 let ball;
+let ballH = [];
 
 function setup() {
   
   createCanvas(WIDTH, HEIGHT);
+  background(0,0,0,255);
+  for(let i = 0; i < 200; i++){
+    ballH.push(new Ball(WIDTH, HEIGHT));
+  }
 
-  ball = new Ball();
 
 }
 
 function draw() {
+  noStroke();
   time += 0.1;
-  background(220);
-  ball.draw();
+  background(0,0,0,7);
+
+  for(let i = 0; i < ballH.length; i++){
+    ballH[i].draw();
+    ballH[i].move();
+    
+  }
+  
   // ellipse(50,50,80,random(50));
   fill(0, 200, 0);
   ellipse((sin(time) * 59) + WIDTH/2,(cos(time) * 59) + HEIGHT/2,80,80);
