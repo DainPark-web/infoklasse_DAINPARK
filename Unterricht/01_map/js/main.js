283d660eeebbae3e7776488a022b79fe30d72500
dain <57316771+DainPark-web@users.noreply.github.com>
2021-11-22 14:49:29 +0100

    update

--- Unterricht/01_map/js/main.js
+++ Unterricht/01_map/js/main.js
@@ -1,7 +1,45 @@
+
+
+
+let bg;
+let r = 200;
+
 function setup(){
     createCanvas(innerWidth, innerHeight);
+    bg = createGraphics(innerWidth, innerHeight);
+
+
+    angleMode(DEGREES);
+    //bg
+    bg.noStroke();
+    bg.background(20, 30);
+    for(let i =0; i < 30000; i++){
+        let x = random(width);
+        let y = random(height);
+        let n = noise(x * 0.01, y * 0.01) * 2;
+        
+        bg.fill(100, 60);
+        bg.rect(x, y, n,n);
+    }
 }
 
 function draw(){
-    background(0);
+    
+    push()
+        translate(width/2, height/2);
+        stroke(255);
+        strokeWeight(20);
+        beginShape(POINTS);
+            for(let i = 0; i < 20; i++){
+                vertex(sin(i * 20) * r, cos(i * 20) * r);
+            }
+
+        endShape();
+    pop();
+
+    image(bg, 0, 0,);
 }
+
+// function windowResized(){
+//     resizeCanvas(innerWidth, innerHeight);
+// }
