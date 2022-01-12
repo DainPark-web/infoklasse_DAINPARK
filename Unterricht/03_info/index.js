0c99536f143d0ff33c5855cac946cf334519526d
dain <57316771+DainPark-web@users.noreply.github.com>
2022-01-12 20:06:24 +0100

    update

---
+++ Unterricht/03_info/index.js
@@ -0,0 +1,6 @@
+// init controller
+var tl = gsap.timeline();
+
+tl.from("#ani-1", {x: -4000,opacity: 0, duration: 1, ease: "power4.out"}, 0.1)
+tl.from("#ani-2", {x: -4000,opacity: 0, duration: 1, ease: "power4.out"}, 0.3)
+tl.from("#ani-3", {x: -4000,opacity: 0, duration: 1, ease: "power4.out"}, 0.5)
